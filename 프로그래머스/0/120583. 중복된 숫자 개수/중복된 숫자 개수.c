e64df7fd1e75a9544a759b319ad2c6242c7b996d
hyeonseog <163272088+hyeonseog@users.noreply.github.com>
2024-05-23 23:00:42 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len, int n) {
+    int sum = 0;
+    for(int i = 0; i < array_len; i++) {
+        if(array[i] == n) {
+            sum += 1;
+        }
+    }
+    int answer = sum;
+    return answer;
+}

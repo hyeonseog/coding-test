a09180cb248ce68c2ecba05ee0bc5749319262ee
정현석 <163272088+hyeonseog@users.noreply.github.com>
2024-06-21 09:45:02 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int answer = 0;
+    answer += n * 12000;
+    answer += k * 2000;
+    for(int i = 0; i < n / 10; i++) {
+        answer -= 2000;
+    }
+    return answer;
+}

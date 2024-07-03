471767ca5101d42ab3500fb4c8da50ba0b38a86e
정현석 <163272088+hyeonseog@users.noreply.github.com>
2024-07-03 14:35:15 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(10000);
+    int len = strlen(my_string);
+    int a = 0;
+    for(int i = 0; i < len; i++) {
+        if(my_string[i] != 'a' && my_string[i] != 'e' && my_string[i] != 'i' && my_string[i] != 'o' && my_string[i] != 'u') {
+            answer[a] = my_string[i];
+            a++;
+        }
+    }
+    return answer;
+}

baee612f1f15f0501b5aefd884f2804f5957fd2d
정현석 <163272088+hyeonseog@users.noreply.github.com>
2024-07-22 19:54:52 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// box_len은 배열 box의 길이입니다.
+int solution(int box[], size_t box_len, int n) {
+    int answer = 1;
+    answer *= box[0] / n;
+    answer *= box[1] / n;
+    answer *= box[2] / n;
+    return answer;
+}

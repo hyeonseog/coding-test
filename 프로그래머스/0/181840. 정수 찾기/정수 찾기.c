1afeae137f3d61f805a674c939abb89577f01ced
정현석 <163272088+hyeonseog@users.noreply.github.com>
2024-09-29 23:30:28 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t a, int n) {
+    int answer = 0;
+    for(int i = 0; i < a; i++) {
+        if(num_list[i] == n) {
+            answer = 1;
+        }
+    }
+    return answer;
+}
